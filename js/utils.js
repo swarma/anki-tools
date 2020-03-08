8d2299eb4c8f93428aca65cc5640df76637634f7
pimgeek <wudi.biz@gmail.com>
2020-03-08 23:17:19 +0800

    1. 为大部分方法添加代码注释 2. 做一些必要的变量重命名和方法位置调整，使整个脚本的逻辑顺序更清晰 3. 发现了死锁的原因，出现在 Text2Outline 方法中，已经改名为 Text2LeveledObj，准备下次提交时修改

--- js/utils.js
+++ js/utils.js
@@ -1,79 +1,12 @@
-// 全局正则替换函数
+// 正则替换函数（替换所有）
 function regexReplG(orig_str, regex_str, subst_str) {
   const regex = new RegExp(regex_str, 'g');
   return orig_str.replace(regex, subst_str);
 }
-
+// 正则替换函数（替换一处）
 function regexRepl(orig_str, regex_str, subst_str) {
   const regex = new RegExp(regex_str);
   return orig_str.replace(regex, subst_str);
-}
-// 读取本地文件内容
-function readLocalFile(e) {
-  var file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var opml_text = e.target.result;
-    var escaped_opml_text = opmlTextEscape(opml_text);
-    var json_obj = OPML2JSON(escaped_opml_text);
-    var markdown_str = JSON2Markdown(json_obj);
-    // Display file content
-    displayContents(markdown_str);
-  };
-  reader.readAsText(file);
-}
-// 对多行文本内容做必要的预处理
-function preProcess(text) {
-  var res_str = text;
-  res_str = regexReplG(res_str.trim(), '&nbsp;', ' ');
-  res_str = regexReplG(res_str.trim(), '^[\r\n]+', '');
-  res_str = regexReplG(res_str.trim(), '[\r\n]+$', '');
-  res_str = regexReplG(res_str.trim(), '[\r\n]+', '\n');
-  return res_str;
-}
-function encodeHTML(text) {
-  var textarea = document.createElement("textarea");
-  textarea.textContent = text;
-  return textarea.innerHTML;
-}
-function decodeHTML(html) {
-  var textarea = document.createElement("textarea");
-  textarea.innerHTML = html;
-  return textarea.value;
-}
-function remove_html_link_tag(text) {
-  var res_str = text;
-  res_str = regexReplG(res_str.trim(), '<a class="content-link"[^>]+>([^<]+)</a>', '$1');
-  return res_str;
-}
-function opmlTextEscape(opml_text) {
-  const regex = '<outline text="([^"]+)"';
-  var res = opml_text.match(regex);
-  while(res) {
-    subst = encodeHTML(res[1]);
-    opml_text = regexRepl(opml_text.trim(), regex, '<outline pimgeek_text="' + subst + '"');
-    res = opml_text.match(regex);
-  }
-  opml_text = regexReplG(opml_text, '<outline pimgeek_text=', '<outline text=');
-  return opml_text;
-}
-// 网页文本下载
-function download(filename, text) {
-  var pom = document.createElement('a');
-  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  pom.setAttribute('download', filename);
-  
-  if (document.createEvent) {
-    var event = document.createEvent('MouseEvents');
-    event.initEvent('click', true, true);
-    pom.dispatchEvent(event);
-  }
-  else {
-    pom.click();
-  }
 }
 // 同步设置 TEXTAREA 编辑框内容
 function setNativeValue(element, value) {
@@ -89,13 +22,71 @@ function setNativeValue(element, value) {
     throw new Error('指定的元素没有 setter.');
   }
 }
-function displayContents(contents) {
-  var element = document.getElementById('markdown');
+// 把指定内容写入 TEXTAREA
+function fillTextArea(textarea_id, contents) {
+  var element = document.getElementById(textarea_id);
   setNativeValue(element, contents);
   element.dispatchEvent(new Event('input', { bubbles: true }));
 }
-// 把用户输入的原始文本转换为 Outline 对象以便做后续处理
-function text2Outline(text) {
+// 读取本地文件内容并做处理
+function readLocalFile(e) {
+  var file = e.target.files[0];
+  var textarea_id = e.target.textarea_id;
+  if (!file) {
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var opml_text = e.target.result;
+    var escaped_opml_text = opmlTextEscape(opml_text);
+    var json_obj = OPML2JSON(escaped_opml_text);
+    var markdown_str = JSON2Markdown(json_obj);
+    // 读取出文件内容立即写入 TEXTAREA 区域
+    fillTextArea(textarea_id, markdown_str);
+  };
+  reader.readAsText(file);
+}
+// 对多行文本内容的预处理动作
+function preProcess(text) {
+  var res_str = text;
+  res_str = regexReplG(res_str.trim(), '&nbsp;', ' ');
+  res_str = regexReplG(res_str.trim(), '^[\r\n]+', '');
+  res_str = regexReplG(res_str.trim(), '[\r\n]+$', '');
+  res_str = regexReplG(res_str.trim(), '[\r\n]+', '\n');
+  return res_str;
+}
+// 对文本内容中的 > < 等符号做编码处理
+function encodeHTML(text) {
+  var textarea = document.createElement("textarea");
+  textarea.textContent = text;
+  return textarea.innerHTML;
+}
+// 对文本内容中的 &gt; &lt; 等符号做解码处理
+function decodeHTML(html) {
+  var textarea = document.createElement("textarea");
+  textarea.innerHTML = html;
+  return textarea.value;
+}
+// 移除大纲文本内容中已有的超链接（针对 mubu.io）
+function remove_html_link_tag(text) {
+  var res_str = text;
+  res_str = regexReplG(res_str.trim(), '<a class="content-link"[^>]+>([^<]+)</a>', '$1');
+  return res_str;
+}
+// 把 opml 中的 text 属性改名为 pimgeek_text（针对 mubu.io）
+function opmlTextEscape(opml_text) {
+  const regex = '<outline text="([^"]+)"';
+  var res = opml_text.match(regex);
+  while(res) {
+    subst = encodeHTML(res[1]);
+    opml_text = regexRepl(opml_text.trim(), regex, '<outline pimgeek_text="' + subst + '"');
+    res = opml_text.match(regex);
+  }
+  opml_text = regexReplG(opml_text, '<outline pimgeek_text=', '<outline text=');
+  return opml_text;
+}
+// 把用户输入的原始文本转换为 LeveledObj 对象以便做后续处理
+function text2LeveledObj(text) {
   var clean_text = preProcess(text);
   var lines = clean_text.split("\n");  
   var outline = [];
@@ -124,14 +115,12 @@ function JSON2Markdown(json_obj) {
   var markdown_str = convItemToMarkdownArrayByLevel(root_item, 12).join('\n');
   return markdown_str;
 }
-
 // 解析幕布专用图片格式
 function imgParse(text) {
   unescaped_text = unescape(text);
   res_str = regexReplG(unescaped_text.trim(), '.*:"(.+\.jpg)"\}\]', '!\[.\]\(https://mubu.com/$1\)');
   return res_str;
 }
-
 // 获取指定大纲条目的标题
 function getItemTitle(item){
   res_str = ''
@@ -167,12 +156,12 @@ function getInsideItems(item){
     return [];
   }
 }
-// 获取
+// 把给定的对象逐级转换为 Markdown 文本
 function convItemToMarkdownArrayByLevel(item, level) {
   var md_array = [];
   var sub_md_array = [];
   if (item === 'undefined' ||('__complete' in item && item['__complete'] === 'true')) {
-    return []; // 边界条件
+    return []; // 妥善处理边界条件，避免出现异常
   }
   else if (level < 0 || level > 50) {
     console.log("level 数值超出范围（0-50）！");
@@ -211,11 +200,11 @@ function markdown2HTML(input_str) {
   output_str = regexReplG(output_str, '\\!\\[[^\[]*\\]\\(([^\(\)]*)\\)', '<img src="$1">');
   return output_str;
 }
-// 从给定的 Outline 中识别某节点的直接父节点
+// 从给定的 LeveledObj 中识别某节点的直接父节点
 // 并返回该父节点的名称
-function getParentTitle(outline, child_title) {
+function getParentTitle(leveled_obj, child_title) {
   let parent_title = "";
-  for (let item of outline) {
+  for (let item of leveled_obj) {
     if (item.hasOwnProperty('children') && 
       item.children.indexOf(child_title) >= 0) {
       parent_title = item.title;
@@ -224,13 +213,13 @@ function getParentTitle(outline, child_title) {
   }
   return parent_title;
 }
-// 从给定的 Outline 中查找某级别的上级节点
+// 从给定的 LeveledObj 中查找某级别的上级节点
 // 并返回其节点 id
 // (注意是按照从下往上的顺序查找, 找到第一个就停止寻找)
-function findParent(outline, child_level) {
+function findParent(leveled_obj, child_level) {
   let parent_idx = -1;
-  for (let idx = outline.length - 1; idx >= 0; idx--) {
-    if (outline[idx].level == child_level - 1) {
+  for (let idx = leveled_obj.length - 1; idx >= 0; idx--) {
+    if (leveled_obj[idx].level == child_level - 1) {
       parent_idx = idx;
       break;
     }
@@ -270,7 +259,7 @@ function getAnkiChapterInfo(item, xpath) {
 // 把用户输入的 Markdown 文本转换为 Anki Q&A 格式
 function markdown2QA(markdown_text) {
   var qa_text = "";
-  var outline = text2Outline(markdown_text);
+  var outline = text2LeveledObj(markdown_text);
   
   for (var item of outline) {
     var xpath = getXPathInOutline(outline, item);
@@ -300,7 +289,7 @@ function postProcess(input_str) {
 // 把用户输入的 Markdown 文本转换为 AnkiCSV 导入格式
 function markdown2AnkiCSV(markdown_text) {
   var anki_csv = "";
-  var outline = text2Outline(markdown_text);
+  var outline = text2LeveledObj(markdown_text);
   
   for (var item of outline) {
     let xpath = getXPathInOutline(outline, item);
@@ -320,4 +309,19 @@ function markdown2AnkiCSV(markdown_text) {
     }
   }
   return anki_csv;
+}
+// 点击下载按钮后，利用此方法创建文件并唤起下载动作
+function download(filename, text) {
+  var pom = document.createElement('a');
+  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  pom.setAttribute('download', filename);
+  
+  if (document.createEvent) {
+    var event = document.createEvent('MouseEvents');
+    event.initEvent('click', true, true);
+    pom.dispatchEvent(event);
+  }
+  else {
+    pom.click();
+  }
 }
